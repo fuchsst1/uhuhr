326b773002866e92d1e0975bbb0b8b0fe6ff8c79
stetwig <fuchsst1@students.zhaw.ch>
2018-04-07 11:17:53 +0200

    UPDATE
    Mit Links im Dashboard

--- JS/add_project_form.js
+++ JS/add_project_form.js
@@ -9,3 +9,15 @@ var modal = document.getElementById('id02');
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+
+function startTime(){
+  var sd = Date.parse(new Date)
+  sd /= 6000;
+document.getElementById("startdate").innerHTML = sd;
+}
+
+function endTime(){
+  var ed = Date.parse(new Date)
+  ed /= 6000;
+  document.getElementById("enddate").innerHTML = ed;
+}
