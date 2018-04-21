75956113c1d90d28c1a6d981207c89e503402ca3
stetwig <fuchsst1@students.zhaw.ch>
2018-04-21 18:39:35 +0200

    Update

--- JS/add_project_form.js
+++ JS/add_project_form.js
@@ -1,23 +1,9 @@
+$(document).ready(function() {
 
+$("#register").on("click", function() {
+  $("#hide").show();
+})
 
-var modal = document.getElementById('id01');
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+$("#hide").hide();
 
-var modal = document.getElementById('id02');
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-
-function startTime(){
-  var sd = Date.parse(new Date)
-  sd /= 6000;
-document.getElementById("startdate").innerHTML = sd;
-}
-
-function endTime(){
-  var ed = Date.parse(new Date)
-  ed /= 6000;
-  document.getElementById("enddate").innerHTML = ed;
-}
+})
