891ebc65d7e2d9215ffb6ba671acd972c0525d3a
stetwig <fuchsst1@students.zhaw.ch>
2018-04-22 13:01:43 +0200

    Update

--- JS/add_project_form.js
+++ JS/add_project_form.js
@@ -6,4 +6,48 @@ $("#register").on("click", function() {
 
 $("#hide").hide();
 
+$("input#taskinput").keyup(function() {
+    var value = $( this ).val();
+    $( "p#taskoutput" ).text( value );
+  })
+  .keyup();
+
+$("input#taskinput").keyup(function() {
+    var value = $( this ).val();
+    $("#taskname").val(value);
+  })
+  .keyup();
+
+
+
+$("#save").on("click", function() {
+
+    $("input#taskinput").val(function() {
+            var value = $( this ).val();
+            $("input#sdate").val(function() {
+                    var sdate = $( this ).val();
+                    $("input#sdate").val(function() {
+                            var sdate = $( this ).val();
+
+        $( "table" ).append('<tr><td class="p-name"><a href="#">'+value+ '</a><p id="taskoutput"></p><br><small>Created '+sdate+'</small></td></tr>');
+  })
+  })
+})
+    $("#id02").hide();
+  })
+
+
+
+
+
+
+
+
+  $("#test").click(function(){
+          $("p").append(" <b>Appended text</b>.");
+          $('table').append('<tr><td class="p-name"><a href="#">Schrift rot machen</a><p id="taskoutput"></p><br><small>Created 11.12.2017</small></td></tr>');
+
+      });
+
+
 })
