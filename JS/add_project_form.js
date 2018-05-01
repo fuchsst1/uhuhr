0aa5b93fc55ac52680751e0a3f4b44267296e8ac
stetwig <fuchsst1@students.zhaw.ch>
2018-05-01 17:12:37 +0200

    Revert "Major Update"
    This reverts commit c6494e6a0f00578645ca05d2dcf99081022f802a.

--- JS/add_project_form.js
+++ JS/add_project_form.js
@@ -1,43 +1,53 @@
 $(document).ready(function() {
 
-  $("#register").on("click", function() {
-    $("#hide").show();
+$("#register").on("click", function() {
+  $("#hide").show();
+})
+
+$("#hide").hide();
+
+$("input#taskinput").keyup(function() {
+    var value = $( this ).val();
+    $( "p#taskoutput" ).text( value );
   })
+  .keyup();
 
-  $("#hide").hide();
+$("input#taskinput").keyup(function() {
+    var value = $( this ).val();
+    $("#taskname").val(value);
+  })
+  .keyup();
 
-  $("input#taskinput").keyup(function() {
-      var value = $(this).val();
-      $("#taskname").val(value);
-    })
-    .keyup();
 
-  $("#save").on("click", function() {
 
-    $("input#taskname").val(function() {
-      var value = $(this).val();
-      $("input#sdate").val(function() {
-        var sdate = $(this).val();
-        $("input#sdate").val(function() {
-          var sdate = $(this).val();
-          $("#sel1 option:selected").val(function() {
-            var prio = $(this).val();
+$("#save").on("click", function() {
 
-            $("table").append('<tr><td class="p-name"><a href="#">' + value +
-              '</a><br><small>Created ' + sdate + +prio +
-              '</small></td><td class="p-team"><a href="#"><img alt="image" src="../images/avatar1.png"></a></td><td class="p-progress"><div class="progress progress-xs"><div style="width: 10%;" class="progress-bar progress-bar-success"></div></div><small>10% Complete</small></td><td><span class="label label-default">low</span></td><td><a href="#" class="btn btn-primary btn-xs"><i class="fa fa-folder"></i> View </a><a href="#" class="btn btn-info btn-xs"><i class="fas fa-pencil-alt"></i> Edit </a><a href="#" class="btn btn-danger btn-xs"><i class="far fa-trash-alt"></i> Delete </a></td></tr>');
-          })
-        })
-      })
-    })
+    $("input#taskinput").val(function() {
+            var value = $( this ).val();
+            $("input#sdate").val(function() {
+                    var sdate = $( this ).val();
+                    $("input#sdate").val(function() {
+                            var sdate = $( this ).val();
+
+        $( "table" ).append('<tr><td class="p-name"><a href="#">'+value+ '</a><p id="taskoutput"></p><br><small>Created '+sdate+'</small></td></tr>');
+  })
+  })
+})
     $("#id02").hide();
-
   })
 
-  $("a[href='#page-top']").click(function() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-    return false;
-  });
+
+
+
+
+
+
+
+  $("#test").click(function(){
+          $("p").append(" <b>Appended text</b>.");
+          $('table').append('<tr><td class="p-name"><a href="#">Schrift rot machen</a><p id="taskoutput"></p><br><small>Created 11.12.2017</small></td></tr>');
+
+      });
 
 
 })
