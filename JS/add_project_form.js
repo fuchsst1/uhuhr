a56a02f1e04ed11d045cdea12174ad1f22ff61cf
gigerurs <34299892+gigerurs@users.noreply.github.com>
2018-05-02 14:37:56 +0200

    Update Templating

--- JS/add_project_form.js
+++ JS/add_project_form.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
     return false;
   });
 
-  $(document).ready(function(){
+  /* $(document).ready(function(){
       $("#subproject2").append("<tr>" +
         "<td>" + 'Neue Homepage' + "</td>" +
         "<td>" + 'ABB' + "</td>" +
@@ -61,7 +61,7 @@ $(document).ready(function() {
           '<a href="#" class="btn btn-info btn-xs"><i class="fas fa-pencil-alt"></i> Bearbeiten </a>' +  " " +
           '<a href="#" class="btn btn-danger btn-xs"><i class="far fa-trash-alt"></i> Löschen </a>' + "</td>" +
           "</tr>");
-    });
+    }); */
 
 
 
@@ -88,3 +88,48 @@ $(document).ready(function() {
   })
 
 })
+
+$(document).ready(function () {
+
+    // dummy data
+    var projekte = [
+        {
+            id: 1,
+            name: "Neue Homepage",
+            mitarbeiter: "../images/avatar1.png",
+            kunde: "ABB",
+            startdatum: "21.01.2018",
+            enddatum: "30.06.2018",
+        },
+        {
+            id: 2,
+            name: "Neue Homepage",
+            mitarbeiter: "../images/avatar2.png",
+            kunde: "ABB",
+            startdatum: "21.01.2018",
+            enddatum: "30.06.2018",
+        },
+    ]
+
+
+    // Read template
+    var projekt_list_item_template = $("#projekt_list_item_template").html();
+
+    // Compile Template
+    var compiled_projekt_list_item_template = _.template(projekt_list_item_template);
+
+    // Add each truck
+    for (var item of projekte) {
+        $("#subproject2").append(compiled_projekt_list_item_template(
+            {
+                projekt_id: item.id,
+                projekt_name: item.name,
+                projekt_mitarbeiter: item.mitarbeiter,
+                projekt_kunde: item.kunde,
+                projekt_startdatum: item.startdatum,
+                projekt_enddatum: item.enddatum
+            }
+        ));
+    }
+
+});
