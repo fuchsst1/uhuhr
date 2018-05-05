d9dd7150e9eb9d2b719f8b65503f67e5097582e6
gigerurs <34299892+gigerurs@users.noreply.github.com>
2018-05-05 14:56:44 +0200

    verzweifelter Verusch2

--- JS/add_project_form.js
+++ JS/add_project_form.js
@@ -1,29 +1,28 @@
-  $(document).ready(function(){
+$(document).ready(function(){
+  $("#subproject").on("click", function(){
+    var projektname = $("#user").val();
+    var kunde = $("#sel1").val();
+    var startdatum = $("#startdatum").val();
+    var enddatum = $("#enddatum").val();
+    $("#subproject2").append("<tr>" +
+      "<td>" + projektname + "</td>" +
+      "<td>" +
+      '<a class="p-team" href="../images/avatar3.png"> <img alt="image" src="../images/avatar3.png" width="20px" height="20px"> </a>' +
+      "</td>" +
+      "<td>" + kunde + "</td>" +
+      "<td>" + startdatum + "</td>" +
+      "<td>" + enddatum + "</td>" +
+      "<td>" +
+      '<a href="project_details.html" class="btn btn-primary btn-xs"><i class="fa fa-folder"></i> Anzeigen </a>' + " " +
+      '<a href="#" class="btn btn-info btn-xs"><i class="fas fa-pencil-alt"></i> Bearbeiten </a>' +  " " +
+      '<a href="#" class="btn btn-danger btn-xs"><i class="far fa-trash-alt"></i> Löschen </a>' + "</td>" +
+      "</tr>");
+  });
+})
 
-    $("#subproject").on("click", function(){
-          var projektname = $("#user").val();
-          var kunde = $("#sel1").val();
-          var startdatum = $("#startdatum").val();
-          var enddatum = $("#enddatum").val();
-          $("#subproject2").append("<tr>" +
-            "<td>" + projektname + "</td>" +
-            "<td>" +
-            '<a class="p-team" href="../images/avatar3.png"> <img alt="image" src="../images/avatar3.png" width="20px" height="20px"> </a>' +
-            "</td>" +
-            "<td>" + kunde + "</td>" +
-            "<td>" + startdatum + "</td>" +
-            "<td>" + enddatum + "</td>" +
-            "<td>" +
-            '<a href="project_details.html" class="btn btn-primary btn-xs"><i class="fa fa-folder"></i> Anzeigen </a>' + " " +
-            '<a href="#" class="btn btn-info btn-xs"><i class="fas fa-pencil-alt"></i> Bearbeiten </a>' +  " " +
-            '<a href="#" class="btn btn-danger btn-xs"><i class="far fa-trash-alt"></i> Löschen </a>' + "</td>" +
-            "</tr>");
-        });
 
 
 $("a[href='#page-top']").click(function() {
-  $("html, body").animate({ scrollTop: 0 }, "slow");
-  return false;
-});
-
+$("html, body").animate({ scrollTop: 0 }, "slow");
+return false;
 });
